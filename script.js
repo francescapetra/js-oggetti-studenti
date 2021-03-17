fc2a624cdc3ac29d3b2e9ed2907ff05894578c02
francescapetra <francescalaurapetra@gmail.com>
2021-03-17 14:19:13 +0100

    creazione cartelle

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// clicco su domanda e apre e chiude
+// $("div").click(
+//   function(){
+//     $(this).children().toggle();
+//     console.log($(this).children("p"));
+//   }
+// )
+//
+// $("li").click(function () {
+//      $(this).css("color", "orange");
+//      console.log($(this));
+//  });
