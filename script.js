17ca98e56bfcaa237a0534c0e8e0dbe7b1497de1
francescapetra <francescalaurapetra@gmail.com>
2021-03-17 15:38:40 +0100

    esercizio 3

--- script.js
+++ script.js
@@ -30,5 +30,32 @@ var studenti = [
 for (var i = 0; i < studenti.length; i++) {
   console.log(studenti[i]);
 }
+
+
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo
 // nell’ordine: nome, cognome e età.
+//
+  var studenti2 = [{
+      nome2: "maria",
+      cognome2: "rossi",
+      eta2: 22
+    },
+    {
+      nome2: "mario",
+      cognome2: "blu",
+      eta2: 20
+    }
+  ]
+
+  // Crea un nuovo profilo e inseriscilo
+  var nuovoStudente2 = prompt("Hai già un account?")
+  if (nuovoStudente2 === "no") {
+    var newUser = prompt("inserisci il tuo nome");
+    var newAccount = {nome2: newUser};
+    newAccount.cognome2 = prompt("inserisci il tuo cognome:");
+    newAccount.eta2 = prompt("inserisci la tua eta:");
+    studenti2.push(newAccount);
+  };
+
+  // tutti gli newAccount
+  console.log(studenti2);
